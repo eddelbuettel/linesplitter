f65bac5d15b6c534fb5f41215966f9a3e4bfcff9
Dirk Eddelbuettel <edd@debian.org>
2023-11-23 07:01:24 -0600

    Simplify to not require inst/include/linesplitter_types.h

--- inst/include/linesplitter_types.h
+++
@@ -1,2 +0,0 @@
-
-#include <nanoarrow/nanoarrow.h>
--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -20,16 +20,19 @@ bool linesplit_from_R_plain(const std::string str, SEXP sxparr) {
     return true;
 }
 
-//' Alternate nice C++ Interface
-//' @noRd
-// [[Rcpp::export]]
-bool linesplit_from_R(const std::string str, Rcpp::XPtr<ArrowArray> sxparr) {
-    // We get an R-created 'nanoarrow_array', an S3 class around an external pointer
-    // Rf_inherits gets the S3 class string, and .inherits() uses that
-    assert_xptr_inherits(sxparr, "nanoarrow_array");
-    // It is a straight up external pointer so we can use R_ExternalPtrAddr() or .get()
-    // struct ArrowArray* arr = (struct ArrowArray*)R_ExternalPtrAddr(sxparr);
-    auto res = linesplitter_read(str, sxparr.get());
+// //' Alternate nice C++ Interface
+// //'
+// //' NB This requires a file inst/include/linesplitter_types.h to define ArrowArray
+// //' for use in the RcppExport wrappers.
+// //' @noRd
+// // [[Rcpp::export]]
+// bool linesplit_from_R(const std::string str, Rcpp::XPtr<ArrowArray> sxparr) {
+//     // We get an R-created 'nanoarrow_array', an S3 class around an external pointer
+//     // Rf_inherits gets the S3 class string, and the helper function use that.
+//     assert_xptr_inherits(sxparr, "nanoarrow_array");
+//     // It is a straight up external pointer so we can use R_ExternalPtrAddr() or .get()
+//     // struct ArrowArray* arr = (struct ArrowArray*)R_ExternalPtrAddr(sxparr);
+//     auto res = linesplitter_read(str, sxparr.get());
 
-    return true;
-}
+//     return true;
+// }
