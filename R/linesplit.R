f6e227b9ed526cc2ec8672f272652c37fecd83e4
Dewey Dunnington <dewey@fishandwhistle.net>
2023-12-26 20:46:17 -0400

    fix array init

--- R/linesplit.R
+++ R/linesplit.R
@@ -13,13 +13,15 @@ linesplit <- function(str, return_as_arrow=FALSE) {
     stopifnot("str must be character" = is.character(str),
               "return_as_arrow must be logical" = is.logical(return_as_arrow))
 
-    arr <- nanoarrow_array_init(na_string())
-    stopifnot("arr is valid pointer" = nanoarrow_pointer_is_valid(arr),
+    arr <- nanoarrow_allocate_array()
+    stopifnot("arr does not already hold a value" = !nanoarrow_pointer_is_valid(arr),
               "arr is nanoarrow_array" = inherits(arr, "nanoarrow_array"))
 
     ##linesplit_impl(str, arr)
     linesplit_from_R_plain(str, arr)
     stopifnot("arr valid after linesplit" = nanoarrow_pointer_is_valid(arr))
+
+    nanoarrow_array_set_schema(arr, na_string())
 
     if (return_as_arrow) {
         stopifnot("return_as_arrow requires 'arrrow'" = requireNamespace("arrow", quietly=TRUE))
